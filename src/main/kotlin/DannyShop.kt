69c84c36bfe2103d60739ab20d89f6e704609b5c
dashaw92 <dashaw92@gmail.com>
2023-01-07 17:16:19 -0500

    Increase example shop size

--- src/main/kotlin/DannyShop.kt
+++ src/main/kotlin/DannyShop.kt
@@ -1,6 +1,10 @@
 package me.danny.shop
 
-import me.danny.shop.data.*
+import Demo.items
+import me.danny.shop.data.Category
+import me.danny.shop.data.DannyShopLoadables
+import me.danny.shop.data.Item
+import me.danny.shop.data.Shop
 import me.danny.shop.inv.Menu
 import me.danny.shop.inv.MenuListener
 import me.danny.shop.inv.ShopMenu
@@ -11,10 +15,6 @@ import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
-
-import me.danny.shop.data.Item
-import me.danny.shop.data.Item.*
-import me.danny.shop.data.Item.ItemType.*
 
 class DannyShop : JavaPlugin() {
     companion object {
@@ -57,48 +57,32 @@ class DannyShop : JavaPlugin() {
     private fun buildSampleShop() {
         Shop.CATEGORIES.clear()
 
-        Shop.CATEGORIES.addAll(listOf(
+        val categories = listOf(
             Category("Ores", Material.DEEPSLATE_GOLD_ORE),
             Category("Nature", Material.PODZOL),
             Category("Wood", Material.SPRUCE_PLANKS),
             Category("Redstone", Material.REDSTONE_BLOCK),
             Category("Building", Material.BIRCH_DOOR),
             Category("Food", Material.SWEET_BERRIES),
-        ))
-
-        val enchPick = me.danny.shop.inv.Item.addEnchantGlow(me.danny.shop.inv.Item.makeItem(Material.DIAMOND_PICKAXE, "${ChatColor.GREEN}Miner's Pickaxe", "${ChatColor.YELLOW}The pickaxe of legend"))
-
-        val items = listOf(
-            Item(IID("pickaxe"),        ItemType.Item(enchPick),        Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Ores")!!),
-            Item(IID("iron_ore"),       Mat(Material.IRON_ORE),         Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Ores")!!),
-            Item(IID("gold_ore"),       Mat(Material.GOLD_ORE),         Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Ores")!!),
-            Item(IID("emerald_ore"),    Mat(Material.EMERALD_ORE),      Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Ores")!!),
-            Item(IID("diamond_ore"),    Mat(Material.DIAMOND_ORE),      Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Ores")!!),
-            Item(IID("exp_10"),         Exp(20.0),                  Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Ores")!!),
-            Item(IID("command"),        Command("give %PLAYER diamond 64"),      Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Ores")!!),
-
-            Item(IID("grass"),          Mat(Material.GRASS_BLOCK),      Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
-            Item(IID("dirt"),           Mat(Material.DIRT),             Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
-            Item(IID("coarse_dirt"),    Mat(Material.COARSE_DIRT),      Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
-            Item(IID("podzol"),         Mat(Material.PODZOL),           Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
-            Item(IID("rooted_dirt"),    Mat(Material.ROOTED_DIRT),      Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
-            Item(IID("warped_nylium"),  Mat(Material.WARPED_NYLIUM),    Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
-            Item(IID("crimson_nylium"), Mat(Material.CRIMSON_NYLIUM),   Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
-            Item(IID("daisy"),          Mat(Material.OXEYE_DAISY),      Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
-            Item(IID("poppy"),          Mat(Material.POPPY),            Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
-            Item(IID("blue_flower"),    Mat(Material.BLUE_ORCHID),      Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
-            Item(IID("purple_flower"),  Mat(Material.ALLIUM),           Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
-            Item(IID("weird_flower"),   Mat(Material.AZURE_BLUET),      Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
-            Item(IID("red_tulip"),      Mat(Material.RED_TULIP),        Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
-            Item(IID("orange_tulip"),   Mat(Material.ORANGE_TULIP),     Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
-            Item(IID("brown_mushroom"), Mat(Material.BROWN_MUSHROOM),   Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
-            Item(IID("red_mushroom"),   Mat(Material.RED_MUSHROOM),     Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
-            Item(IID("white_tulip"),    Mat(Material.WHITE_TULIP),      Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
-            Item(IID("pink_tulip"),     Mat(Material.PINK_TULIP),       Cost(10.0, 5.0), Cooldown.None, Quantities(listOf(1), Quantities.Allowed.Any), Shop.getCategory("Nature")!!),
+//            Category("Test1", Material.STICK),
+//            Category("Test2", Material.STICKY_PISTON),
+//            Category("Test3", Material.PORKCHOP),
         )
+        Shop.CATEGORIES.addAll(categories)
+
+        val items2 = items.map {
+            val out: MutableList<Item> = mutableListOf()
+            for(category in categories) {
+                val newItem = it.copy(category = category)
+                out += newItem
+            }
+            out
+        }
 
         val map: MutableMap<Category, MutableList<Item>> = mutableMapOf()
-        items
+        items2
+            .flatten()
+            .shuffled()
             .groupByTo(map) { it.category }
         SHOP = Shop(map)
 
