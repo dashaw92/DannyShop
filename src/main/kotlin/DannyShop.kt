e08d0b628d3f707811c23bf45625225efc0167a4
dashaw92 <dashaw92@gmail.com>
2023-01-07 21:19:46 -0500

    Remove test shop

--- src/main/kotlin/DannyShop.kt
+++ src/main/kotlin/DannyShop.kt
@@ -1,15 +1,11 @@
 package me.danny.shop
 
 import me.danny.shop.commands.ImportCommand
-import me.danny.shop.data.Category
 import me.danny.shop.data.DannyShopLoadables
-import me.danny.shop.data.Demo.items
-import me.danny.shop.data.Item
 import me.danny.shop.data.Shop
 import me.danny.shop.inv.*
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
-import org.bukkit.Material
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
@@ -40,17 +36,11 @@ class DannyShop : JavaPlugin() {
         }
 
         if(args.isEmpty()) {
-            sender.sendMessage("Usage: /dannyshop <test/open>")
+            ShopMenu(SHOP, sender)
             return true
         }
 
         when (args.first().lowercase()) {
-            "test" -> {
-                buildSampleShop()
-                sender.sendMessage("Done! Check config!")
-            }
-
-            "open" -> ShopMenu(SHOP, sender)
             "import" -> ImportCommand.onCommand(sender, args.sliceArray(2 until args.size))
             "catedit" -> {
                 if (!sender.hasPermission("dannyshop.admin")) {
@@ -62,37 +52,5 @@ class DannyShop : JavaPlugin() {
             }
         }
         return true
-    }
-
-    private fun buildSampleShop() {
-        Shop.CATEGORIES.clear()
-
-        val categories = listOf(
-            Category("Ores", Material.DEEPSLATE_GOLD_ORE),
-            Category("Nature", Material.PODZOL),
-            Category("Wood", Material.SPRUCE_PLANKS),
-            Category("Redstone", Material.REDSTONE_BLOCK),
-            Category("Building", Material.BIRCH_DOOR),
-            Category("Food", Material.SWEET_BERRIES),
-        )
-        Shop.CATEGORIES.addAll(categories)
-
-        val items2 = items.map {
-            val out: MutableList<Item> = mutableListOf()
-            for(category in categories) {
-                val newItem = it.copy(category = category)
-                out += newItem
-            }
-            out
-        }
-
-        val map: MutableMap<Category, MutableList<Item>> = mutableMapOf()
-        items2
-            .flatten()
-            .shuffled()
-            .groupByTo(map) { it.category }
-        SHOP = Shop(map)
-
-        DannyShopLoadables.saveShop(SHOP)
     }
 }
