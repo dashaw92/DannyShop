5d5dcbdd7be3ad05a18f30a6a5f6ccb6736dc44e
dashaw92 <dashaw92@gmail.com>
2023-01-07 22:17:17 -0500

    Move shop command to own class

--- src/main/kotlin/DannyShop.kt
+++ src/main/kotlin/DannyShop.kt
@@ -1,14 +1,12 @@
 package me.danny.shop
 
-import me.danny.shop.commands.ImportCommand
+import ShopCommand
 import me.danny.shop.data.DannyShopLoadables
 import me.danny.shop.data.Shop
-import me.danny.shop.inv.*
+import me.danny.shop.inv.ImportListener
+import me.danny.shop.inv.Menu
+import me.danny.shop.inv.MenuListener
 import org.bukkit.Bukkit
-import org.bukkit.ChatColor
-import org.bukkit.command.Command
-import org.bukkit.command.CommandSender
-import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 class DannyShop : JavaPlugin() {
@@ -21,36 +19,11 @@ class DannyShop : JavaPlugin() {
         SHOP = DannyShopLoadables.loadShop(this)
         Bukkit.getPluginManager().registerEvents(MenuListener, this)
         Bukkit.getPluginManager().registerEvents(ImportListener, this)
+        getCommand("dannyshop")!!.setExecutor(ShopCommand)
     }
 
     override fun onDisable() {
         DannyShopLoadables.saveShop(SHOP)
         Menu.closeOpenInvs()
-    }
-
-    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        if(label.lowercase() != "dannyshop") return true
-        if(sender !is Player) {
-            sender.sendMessage("${ChatColor.RED}Nope!")
-            return true
-        }
-
-        if(args.isEmpty()) {
-            ShopMenu(SHOP, sender)
-            return true
-        }
-
-        when (args.first().lowercase()) {
-            "import" -> ImportCommand.onCommand(sender, args.sliceArray(2 until args.size))
-            "catedit" -> {
-                if (!sender.hasPermission("dannyshop.admin")) {
-                    sender.sendMessage("${ChatColor.RED}You lack permission.")
-                    return true
-                }
-
-                CategoryEditor(sender)
-            }
-        }
-        return true
     }
 }
