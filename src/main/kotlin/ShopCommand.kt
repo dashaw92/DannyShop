0d9b0d1969fe03dbe5cfbf5d314465691686ee08
dashaw92 <dashaw92@gmail.com>
2023-01-07 22:18:26 -0500

    Command in own class

---
+++ src/main/kotlin/ShopCommand.kt
@@ -0,0 +1,37 @@
+import me.danny.shop.DannyShop
+import me.danny.shop.commands.ImportCommand
+import me.danny.shop.inv.CategoryEditor
+import me.danny.shop.inv.ShopMenu
+import org.bukkit.ChatColor
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+object ShopCommand : CommandExecutor {
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (sender !is Player) {
+            sender.sendMessage("${ChatColor.RED}Nope!")
+            return true
+        }
+
+        if (args.isEmpty()) {
+            ShopMenu(DannyShop.SHOP, sender)
+            return true
+        }
+
+        when (args.first().lowercase()) {
+            "import" -> ImportCommand.onCommand(sender, args.sliceArray(2 until args.size))
+            "catedit" -> {
+                if (!sender.hasPermission("dannyshop.admin")) {
+                    sender.sendMessage("${ChatColor.RED}You lack permission.")
+                    return true
+                }
+
+                CategoryEditor(sender)
+            }
+        }
+        return true
+    }
+
+}
