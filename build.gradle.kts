433fe3101ae976d06f6ed2dbb3509055d6344375
dashaw92 <dashaw92@gmail.com>
2024-12-19 12:16:20 -0500

    Add more vars to plugin.yml to minimize maintenance costs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,15 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
+val spigotVersion = "1.21.4-R0.1-SNAPSHOT"
+val spigotAPIVersion = "1.21"
+val kotlinVersion = "2.1.0"
+val configurateVersion = "4.1.2"
+val mongoDriverVersion = "5.2.1"
+val essentialsVersion = "2.20.1"
+val vaultVersion = "1.7.1"
+val inputAPIHash = "e0422fa"
+
 plugins {
     `java-library`
     id("org.ajoberstar.grgit") version ("5.3.0")
@@ -9,7 +18,6 @@ plugins {
 
 group = "me.daniel"
 version = "${getHash()}-dev"
-val spigotVersion = "1.21.4-R0.1-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_23
@@ -28,11 +36,11 @@ repositories {
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:$spigotVersion")
-    compileOnly("org.spongepowered:configurate-yaml:4.1.2")
-    compileOnly("net.essentialsx:EssentialsX:2.20.1")
-    compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
-    implementation("org.mongodb:mongodb-driver-sync:5.2.1")
-    implementation(files("libs/InputAPI-e0422fa-dev.jar"))
+    compileOnly("org.spongepowered:configurate-yaml:$configurateVersion")
+    compileOnly("net.essentialsx:EssentialsX:$essentialsVersion")
+    compileOnly("com.github.MilkBowl:VaultAPI:$vaultVersion")
+    implementation("org.mongodb:mongodb-driver-sync:$mongoDriverVersion")
+    implementation(files("libs/InputAPI-$inputAPIHash-dev.jar"))
     implementation(kotlin("stdlib"))
 }
 
@@ -40,7 +48,13 @@ fun getHash() = grgit.head().abbreviatedId ?: "unknown"
 
 tasks.named<Copy>("processResources") {
     filesMatching("plugin.yml") {
-        expand("git_commit" to version)
+        expand(
+            "git_commit" to version,
+            "kotlin_version" to kotlinVersion,
+            "spigot_api" to spigotAPIVersion,
+            "configurate_version" to configurateVersion,
+            "mongo_version" to mongoDriverVersion
+        )
     }
 }
 
