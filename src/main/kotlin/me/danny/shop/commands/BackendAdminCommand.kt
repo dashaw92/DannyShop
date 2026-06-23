0d85cdf9357dbe6d2284feea8ff3b9f9b64aa0ca
dashaw92 <dashaw92@gmail.com>
2026-06-23 09:24:57 -0400

    Save all data, not just the shop

--- src/main/kotlin/me/danny/shop/commands/BackendAdminCommand.kt
+++ src/main/kotlin/me/danny/shop/commands/BackendAdminCommand.kt
@@ -1,9 +1,6 @@
 package me.danny.shop.commands
 
 import me.danny.shop.DannyShop
-import me.danny.shop.backend.BackendManager
-import me.danny.shop.backend.BackendType
-import me.danny.shop.backend.ShopBackend
 import me.danny.shop.pluginMsg
 import org.bukkit.command.CommandSender
 
@@ -30,66 +27,68 @@ internal object BackendAdminCommand {
         return true
     }
 
-    private fun saveCommand(sender: CommandSender) {
-        save(sender)
-    }
+//    private fun saveCommand(sender: CommandSender) {
+//        save(sender)
+//    }
 
-    private fun migrationCommand(sender: CommandSender, args: Array<out String>) {
-        val toBackend = when (args.first().lowercase()) {
-            "yaml" -> BackendType.Yaml
-            else -> {
-                sender.pluginMsg("&cUnrecognized backend type. Supported: &7yaml&c.")
-                return
-            }
-        }
-
-        migrate(sender, toBackend)
-    }
+//    private fun migrationCommand(sender: CommandSender, args: Array<out String>) {
+//        val toBackend = when (args.first().lowercase()) {
+//            "yaml" -> BackendType.Yaml
+//            else -> {
+//                sender.pluginMsg("&cUnrecognized backend type. Supported: &7yaml&c.")
+//                return
+//            }
+//        }
+//
+//        migrate(sender, toBackend)
+//    }
 
     private fun save(sender: CommandSender) {
         val backend = DannyShop.instance().backend
         sender.pluginMsg("Attempting to save shop data via backend &e${backend.type()}&7...")
         DannyShop.instance().logger.info("${sender.name} invoked a manual save.")
         backend.saveShop(DannyShop.instance(), DannyShop.SHOP)
+        DannyShop.instance().ecolog.save()
+        DannyShop.instance().analytics.save()
         sender.pluginMsg("Save complete.")
     }
 
-    private fun migrate(sender: CommandSender, target: BackendType): ShopBackend? {
-        val pl = DannyShop.instance()
-        if (pl.backend.type() == target) {
-            sender.pluginMsg("&cCannot migrate to that backend: Already in use!")
-            return null
-        }
-
-        sender.pluginMsg("Migrating shop data to &e$target&7...")
-        DannyShop.instance().logger.info("${sender.name} invoked a migration (from=${pl.backend.type()}, to=$target)")
-        val targetProvider = BackendManager.getProvider(pl, target)
-        targetProvider.saveShop(pl, DannyShop.SHOP)
-        sender.pluginMsg("If all went to plan, the shop has been saved via backend &e$target&7!")
-        DannyShop.instance().logger.info("Migration complete")
-        return targetProvider
-    }
-
-    private fun switchCommand(sender: CommandSender, args: Array<out String>) {
-        val toBackend = when (args.first().lowercase()) {
-            "yaml" -> BackendType.Yaml
-            else -> {
-                sender.pluginMsg("&cUnrecognized backend type. Supported: &7yaml&c, &7mongo&c.")
-                return
-            }
-        }
-
-        val pl = DannyShop.instance()
-        sender.pluginMsg("&d1/3&7: Saving current data...")
-        save(sender)
-        sender.pluginMsg("&d2/3&7: Migrating data to &e$toBackend&7...")
-        val newProvider = migrate(sender, toBackend)
-        if (newProvider == null) {
-            sender.pluginMsg("&cFailed to migrate.")
-            return
-        }
-
-        pl.backend = newProvider
-        sender.pluginMsg("&d3/3&7: Switch complete. Now using &e${newProvider.type()}&7 for the backend.")
-    }
+//    private fun migrate(sender: CommandSender, target: BackendType): ShopBackend? {
+//        val pl = DannyShop.instance()
+//        if (pl.backend.type() == target) {
+//            sender.pluginMsg("&cCannot migrate to that backend: Already in use!")
+//            return null
+//        }
+//
+//        sender.pluginMsg("Migrating shop data to &e$target&7...")
+//        DannyShop.instance().logger.info("${sender.name} invoked a migration (from=${pl.backend.type()}, to=$target)")
+//        val targetProvider = BackendManager.getProvider(pl, target)
+//        targetProvider.saveShop(pl, DannyShop.SHOP)
+//        sender.pluginMsg("If all went to plan, the shop has been saved via backend &e$target&7!")
+//        DannyShop.instance().logger.info("Migration complete")
+//        return targetProvider
+//    }
+//
+//    private fun switchCommand(sender: CommandSender, args: Array<out String>) {
+//        val toBackend = when (args.first().lowercase()) {
+//            "yaml" -> BackendType.Yaml
+//            else -> {
+//                sender.pluginMsg("&cUnrecognized backend type. Supported: &7yaml&c, &7mongo&c.")
+//                return
+//            }
+//        }
+//
+//        val pl = DannyShop.instance()
+//        sender.pluginMsg("&d1/3&7: Saving current data...")
+//        save(sender)
+//        sender.pluginMsg("&d2/3&7: Migrating data to &e$toBackend&7...")
+//        val newProvider = migrate(sender, toBackend)
+//        if (newProvider == null) {
+//            sender.pluginMsg("&cFailed to migrate.")
+//            return
+//        }
+//
+//        pl.backend = newProvider
+//        sender.pluginMsg("&d3/3&7: Switch complete. Now using &e${newProvider.type()}&7 for the backend.")
+//    }
 }
