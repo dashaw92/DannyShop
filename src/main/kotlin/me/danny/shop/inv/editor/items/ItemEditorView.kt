841692bac434573b2cbe4701f55a0a3537bd0e80
dashaw92 <dashaw92@gmail.com>
2026-07-07 18:55:49 -0400

    Streamline item property editing by asking directly from the main editor menu

--- src/main/kotlin/me/danny/shop/inv/editor/items/ItemEditorView.kt
+++ src/main/kotlin/me/danny/shop/inv/editor/items/ItemEditorView.kt
@@ -1,15 +1,19 @@
 package me.danny.shop.inv.editor.items
 
 import me.danny.shop.DannyShop
-import me.danny.shop.inv.editor.items.properties.*
+import me.danny.shop.input.Input
+import me.danny.shop.input.askInput
+import me.danny.shop.input.collapse
+import me.danny.shop.inv.editor.items.properties.ItemCategoryEditor
+import me.danny.shop.inv.shop.ShopMenu
 import me.danny.shop.inv.view.MenuView
 import me.danny.shop.inv.view.ViewAction
-import me.danny.shop.inv.shop.ShopMenu
 import me.danny.shop.model.Item
 import me.danny.shop.pluginMsg
 import me.danny.shop.utils.ItemBuilder
 import me.danny.shop.utils.fill
 import org.bukkit.Material
+import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.Inventory
 
@@ -32,19 +36,16 @@ internal class ItemEditorView(private val editor: ItemEditor) : MenuView {
             "  &3CID: &7${item.category.cid.id}",
         )
 
-        val footer = arrayOf("", "&e[Edit: Click]")
-
-        val costButton = ItemBuilder.makeItem(Material.EMERALD, "&ePricing", *cost(item), *footer)
-        val nameButton = ItemBuilder.makeItem(Material.NAME_TAG, "&eName", *name(item), *footer)
+        val costButton = ItemBuilder.makeItem(Material.EMERALD, "&ePricing", *cost(item))
+        val nameButton = ItemBuilder.makeItem(Material.NAME_TAG, "&eName", *name(item))
         val categoryButton = ItemBuilder.makeItem(
             Material.CHEST,
             "&eCategory",
-            "&7${item.category.name}",
-            *footer
+            "&7${item.category.name}"
         )
         val sellLimitButton = ItemBuilder.makeItem(
             Material.IRON_DOOR,
-            "&eSell Limit", *sellLimit(item), *footer
+            "&eSell Limit", *sellLimit(item)
         )
 
         val deleteButton = ItemBuilder.makeItem(
@@ -77,11 +78,72 @@ internal class ItemEditorView(private val editor: ItemEditor) : MenuView {
             return ViewAction.Pass
         }
 
+        val player = event.whoClicked as Player
+        val item = DannyShop.SHOP.itemByIid(editor.item) ?: return ViewAction.Pass
         val newView = when (event.currentItem!!.type) {
-            Material.EMERALD -> CostPropEditor(editor)
             Material.CHEST -> ItemCategoryEditor(editor)
-            Material.NAME_TAG -> ItemNameEditor(editor)
-            Material.IRON_DOOR -> SellLimitEditor(editor)
+            Material.EMERALD -> {
+                fun updateCost(cost: Double?) {
+                    val cost =
+                        if (cost != null && cost > 0) Item.Cost.Value(cost)
+                        else Item.Cost.NotSet
+                    DannyShop.SHOP.replaceItem(item.iid, item.copy(cost = cost))
+                    build(inv)
+                }
+
+                if (event.click.isRightClick) {
+                    updateCost(null)
+                    return ViewAction.Pass
+                }
+
+                player.closeInventory()
+                askInput("&eSet sell price")
+                    .getInput(player) { pl, input -> setPrice(pl, input, ::updateCost, event.inventory) }
+                return ViewAction.Pass
+            }
+
+            Material.NAME_TAG -> {
+                fun updateName(name: String?) {
+                    DannyShop.SHOP.replaceItem(item.iid, item.copy(name = name))
+                    build(inv)
+                }
+
+                if (event.click.isRightClick) {
+                    updateName(null)
+                    return ViewAction.Pass
+                }
+
+                player.closeInventory()
+                askInput("&9New name")
+                    .getInput(player) { pl, input ->
+                        updateName(input.collapse())
+                        pl.openInventory(event.inventory)
+                    }
+                return ViewAction.Pass
+            }
+
+            Material.IRON_DOOR -> {
+                fun updateLimit(limit: ULong) {
+                    val limit: Item.SellLimit = if (limit == 0UL) Item.SellLimit.None
+                    else Item.SellLimit.Amount(limit)
+
+                    DannyShop.SHOP.replaceItem(item.iid, item.copy(sellLimit = limit))
+                    build(inv)
+                }
+
+                if (event.click.isRightClick) {
+                    updateLimit(0UL)
+                    return ViewAction.Pass
+                }
+
+                player.closeInventory()
+                askInput("&eSet sell limit")
+                    .getInput(player) { pl, input ->
+                        setLimit(pl, input, ::updateLimit, event.inventory)
+                    }
+                return ViewAction.Pass
+            }
+
             else -> null
         } ?: return ViewAction.Pass
         return ViewAction.ChangeView(newView)
@@ -100,11 +162,16 @@ internal class ItemEditorView(private val editor: ItemEditor) : MenuView {
                 "&cNot set!",
                 "",
                 "&c&oPlayers cannot sell this!",
-                "&7&oSet a price to fix!"
+                "&7&oSet a price to fix!",
+                "",
+                "&e[Edit: Click]"
             )
 
             is Item.Cost.Value -> arrayOf(
-                "&9Each: &7\$%,.2f".format(item.cost.buy),
+                "&9Each: &7%,.2f".format(item.cost.buy),
+                "",
+                "&e[Remove: Right click]",
+                "&e[Edit: Click]"
             )
         }
     }
@@ -114,13 +181,18 @@ internal class ItemEditorView(private val editor: ItemEditor) : MenuView {
             null -> arrayOf(
                 "&cNo name set",
                 "",
-                "&7&oThis item cannot be searched for by name."
+                "&7&oThis item cannot be searched for by name.",
+                "",
+                "&e[Edit: Click]"
             )
 
             else -> arrayOf(
                 "&7${item.name}",
                 "",
-                "&7&oPlayers can search for this item by that name."
+                "&7&oPlayers can search for this item by that name.",
+                "",
+                "&e[Remove: Right click]",
+                "&e[Edit: Click]"
             )
         }
     }
@@ -130,15 +202,51 @@ internal class ItemEditorView(private val editor: ItemEditor) : MenuView {
             is Item.SellLimit.None -> arrayOf(
                 "&cNo sell limit set.",
                 "",
-                "&7&oPlayers can sell an unlimited amount of this item."
+                "&7&oPlayers can sell an unlimited amount of this item.",
+                "",
+                "&e[Edit: Click]"
             )
 
             is Item.SellLimit.Amount -> arrayOf(
                 "&9Sell limit: &7${item.sellLimit.amount}",
                 "",
                 "&7&oPlayers are limited to selling",
-                "&7&othis many units in a refresh window."
+                "&7&othis many units in a refresh window.",
+                "",
+                "&e[Remove: Right click]",
+                "&e[Edit: Click]"
             )
         }
+    }
+
+    private fun setLimit(player: Player, input: Input, setter: (ULong) -> Unit, inv: Inventory) {
+        val line = input.collapse()
+
+        val limit = line.toIntOrNull()
+        if (limit != null && limit > 0) {
+            setter(limit.toULong())
+            player.pluginMsg("Sell limit set to $limit.")
+        } else {
+            setter(0u)
+            player.pluginMsg("Sell limit removed.")
+        }
+
+        build(inv)
+        player.openInventory(inv)
+    }
+
+    private fun setPrice(player: Player, input: Input, setter: (Double) -> Unit, inv: Inventory) {
+        val line = input.collapse()
+
+        val price = line.toDoubleOrNull()
+        if (price != null && price.isFinite() && price >= 0.0) {
+            setter(price)
+            player.pluginMsg("Price set to &2$%,.2f&7.".format(price))
+        } else {
+            player.pluginMsg("&cUnable to read price from \"&d$line&c\"...")
+        }
+
+        build(inv)
+        player.openInventory(inv)
     }
 }
